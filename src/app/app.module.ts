08a8568278126f3b823926a7e0929b6638e44390
Tsiry Andriamampianina RADIASON <taradiason@ASTEKMTIUS>
2018-02-21 13:30:32 +0400

    integrating google map API with demo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { NgModule } from '@angular/core';
 // -- import bootstrap directives
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+// -- import google map plugin: you need a google API Key
+import { AgmCoreModule } from '@agm/core';
+
 
 import { AppComponent } from './app.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleMapComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCLHISIRyFKWJ9XdCJ5-5S1zXZe9WywEGw'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/google-map/google-map.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.css']
+})
+export class GoogleMapComponent implements OnInit {
+
+  private title: string = 'My first AGM project';
+  private lat: number = 51.678418;
+  private lng: number = 7.809007;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
